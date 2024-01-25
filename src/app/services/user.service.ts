8c44520e996d7994af8932e5882c15f537ea3b3d
Kimberly Kohel-Hayes <kohelka@gmail.com>
2024-01-24 18:21:58 -0600

    added user service and user module

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { User } from '../modules/user';
+import { UrlService } from './url.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  loggedInUser:User;
+  authHeaders = {'Content-type':'application/json','Token':''};
+  regHeaders = {'Content-type':'application/json'};
+
+  constructor(private url:UrlService) { }
+
+  async login(username:string,password:string): Promise<void> {
+    let credentials = {
+      'username':username,
+      'password':password
+  };
+
+  let resp = await fetch(this.url.url + 'users/auth', {method:'POST',body:JSON.stringify(credentials),
+  headers:this.regHeaders});
+
+if (resp.status===200) {
+  let token = await resp.json();
+  localStorage.setItem('Token', token);
+}
+}
+
+
+}
